a60f064376dc0574e08784e91c7e6a8711e97ee4
Nayzawoo-dev <nayzawoo.ace@gmail.com>
2025-05-31 23:53:17 +0630

    feat: AddPostApi

--- RestfulWebApi/Controllers/PersonController.cs
+++ RestfulWebApi/Controllers/PersonController.cs
@@ -36,18 +36,49 @@ namespace RestfulWebApi.Controllers
             return Ok(data);
         }
 
-        [HttpGet("Detail")]
-        [HttpGet("{password}")]
+        [HttpGet("Detail/{password}")]
+        //[HttpGet("{password}/{username}")]
         public IActionResult GetDetailPerson(string password)
         {
-            _dapperservices.Query<PersonModels>("select * from Tbl_Window where Password = @Password",new )
-            return Ok("One Person Detail");
+           var list = _dapperservices.Query<PersonModels>
+                ("select * from Tbl_Window where Password = @Password", new PersonModels
+            {
+                Password = password,
+            });
+            if(list.Count == 0)
+            {
+                var data = new { 
+                Success = false,
+                Message = "Person Not Found!"
+                };
+
+                return NotFound(data);
+            }
+
+            var data2 = new {
+            Success = true,
+            Information = list[0]
+            };
+
+            return Ok(data2);
         }
 
         [HttpPost]
-        public IActionResult CreatePerson()
+        public IActionResult CreatePerson([FromBody] PersonModels person)
         {
-            return Ok("PostPerson");
+            string query = @"INSERT INTO [dbo].[Tbl_Window]
+           ([UserName]
+           ,[Password])
+     VALUES
+           (@UserName
+           ,@Password)";
+            int res = _dapperservices.Execute(query, person);
+            var data = new
+            {
+                Success = res > 0,
+                Message = res > 0 ? "Complete" : "Fail"
+            };
+            return Ok(data);
         }
 
         [HttpPut]
