95b12c4f4e6c65d7bc407522889dc2979f7768ec
Nayzawoo-dev <nayzawoo.ace@gmail.com>
2025-05-31 17:27:29 +0630

    Add project files.

---
+++ DataBaseConnectionSharedLibrary/DapperServices.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlTypes;
+using System.Runtime.InteropServices.ObjectiveC;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace DataBaseConnectionSharedLibrary
+{
+    public class DapperServices
+    {
+        private readonly SqlConnectionStringBuilder _connection;
+
+        public  DapperServices(SqlConnectionStringBuilder connection)
+        {
+            _connection = connection;
+        }
+
+        public List<T> Execute<T>(string query,object parameters = null)
+        {
+            SqlConnection connection = new SqlConnection(_connection.ConnectionString);
+            connection.Open();
+            var lst = connection.Query<T>(query,connection).ToList();
+
+        }
+    }
+}
---
+++ RestfulWebApi/Controllers/PersonController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestfulWebApi.Controllers
+{
+    // https://localhost:3000/api/Person // this is domain and Person is endpoint
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPerson()
+        {
+            return Ok("GetPerson");
+        }
+
+        [HttpGet("Detail")]
+        public IActionResult GetDetailPerson()
+        {
+            return Ok("One Person Detail");
+        }
+
+        [HttpPost]
+        public IActionResult CreatePerson()
+        {
+            return Ok("PostPerson");
+        }
+
+        [HttpPut]
+        public IActionResult CreateOrUpdatePerson()
+        {
+            return Ok("Create or Update Person");
+        }
+
+        [HttpPatch]
+        public IActionResult UpdatePerson()
+        {
+            return Ok("Update Person");
+        }
+
+        [HttpDelete]
+        public IActionResult DeletePerson()
+        {
+            return Ok("Delete Person");
+        }
+    }
+}
